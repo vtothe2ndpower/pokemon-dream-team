561ad1da718141a3660ceed8a915cfa4b326ba8d
Victor Vargas <vvargasnunez@gmail.com>
2020-12-15 16:23:16 -0800

    Updates

---
+++ precache-manifest.6b71624efaa93473a9d15a28e4de9686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef1e07104f58f39823adf99785ae15f",
+    "url": "/pokemon-dream-team/index.html"
+  },
+  {
+    "revision": "1f6c1c2aea2cc803dedc",
+    "url": "/pokemon-dream-team/static/css/main.f54f8745.chunk.css"
+  },
+  {
+    "revision": "6282b3b23b0cb9d3d39c",
+    "url": "/pokemon-dream-team/static/js/2.03b551d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-dream-team/static/js/2.03b551d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f6c1c2aea2cc803dedc",
+    "url": "/pokemon-dream-team/static/js/main.40d0c150.chunk.js"
+  },
+  {
+    "revision": "36132abde8ad4eca60bd",
+    "url": "/pokemon-dream-team/static/js/runtime-main.d1ef22ed.js"
+  }
+]);
