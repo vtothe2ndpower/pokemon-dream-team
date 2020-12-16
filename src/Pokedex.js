a0fa520e51008f42c3a1d2fd81bd0f2bf7cb27f7
Victor Vargas <vvargasnunez@gmail.com>
2020-12-15 16:02:22 -0800

    Add all generations and edit styling/presentation

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,26 +2,20 @@ import React, { Component } from 'react';
 import Pokecard from './Pokecard';
 import './Pokedex.css';
 
-class Pokedex extends Component{
-render(){
-  let title;
-  if(this.props.isWinner){
-    title = <h1 className="Pokedex-winner">WINNER!</h1>
-  } else{
-    title = <h1 className="Pokedex-loser">LOSER!</h1>
-  }
-  return(
-    <div className="Pokedex">
-      {title}
-      <h4>Total Experience: {this.props.exp}</h4>
-      <div className="Pokedex-cards">
-        {this.props.pokemon.map((p) =>(
-        <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />      ))}
-      </div>
-    </div>
-  );
+class Pokedex extends Component {
+	render() {
+		return (
+			<div className="Pokedex">
+				<h2>{this.props.gen}</h2>
+				<h4>Total Experience: {this.props.exp}</h4>
+				<div className="Pokedex-cards">
+					{this.props.pokemon.map((p) => (
+						<Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+					))}
+				</div>
+			</div>
+		);
+	}
 }
-}
-
 
 export default Pokedex;
--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,50 +1,65 @@
 import React, { Component } from 'react';
 import Pokedex from './Pokedex';
 
-class Pokegame extends Component{
-  static defaultProps = {
-    pokemon : [
-      // Generation 1
-      {id: 3, name: 'Venusaur', type: 'Grass/Poison', base_experience: 525},
-      {id: 65, name: 'Alakazam', type: 'Psychic', base_experience: 500},
-      {id: 130, name: 'Gyardos', type: 'Water/Flying', base_experience: 540},
-      {id: 149, name: 'Dragonite', type: 'Dragon/Flying', base_experience: 600},
-      {id: 143, name: 'Snorlax', type: 'Normal', base_experience: 540},
-      {id: 131, name: 'Lapras', type: 'Water/Ice', base_experience: 535},
-      // Generation 2
-      // {id: 181, name: 'Ampharos', type: 'Electric', base_experience: 510},
-      // {id: 208, name: 'Steelix', type: 'Steel/Ground', base_experience: 510},
-      // {id: 229, name: 'Houndoom', type: 'Dark/Fire', base_experience: 500},
-      // {id: 230, name: 'Kingdra', type: 'Water/Dragon', base_experience: 540},
-      // {id: 248, name: 'Tyranitar', type: 'Rock/Dark', base_experience: 600},
-      // {id: 160, name: 'Feraligatr', type: 'Water', base_experience: 530}
-      // Generation 3
-      {id: 254, name: 'Sceptile', type: 'Grass', base_experience: 530},
-      {id: 282, name: 'Gardevoir', type: 'Psychic/Fairy', base_experience: 518},
-      {id: 306, name: 'Aggron', type: 'Steel/Rock', base_experience: 530},
-      {id: 310, name: 'Manectric', type: 'Electric', base_experience: 475},
-      {id: 330, name: 'Flygon', type: 'Ground/Dragon', base_experience: 520},
-      {id: 350, name: 'Milotic', type: 'Water', base_experience: 540}
-    ]
-  };
-  render(){
-    let hand1 = [];
-    let hand2 = [ ...this.props.pokemon ];
-    while(hand1.length < hand2.length){
-      let index = 0;
-      let pokemon = hand2.splice(index, 1)[0];
-      hand1.push(pokemon);
-      index += 1;
-    }
-    let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
-    let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
-    return(
-      <div>
-        <Pokedex pokemon={hand1} exp={exp1} isWinner ={exp1 > exp2}/>
-        <Pokedex pokemon={hand2} exp={exp2} isWinner ={exp1 < exp2}/>
-      </div>
-    );
-  }
+class Pokegame extends Component {
+	static defaultProps = {
+		// Generation 1
+		gen1: [
+			{ id: 3, name: 'Venusaur', type: 'Grass/Poison', base_experience: 525 },
+			{ id: 65, name: 'Alakazam', type: 'Psychic', base_experience: 500 },
+			{ id: 130, name: 'Gyardos', type: 'Water/Flying', base_experience: 540 },
+			{ id: 149, name: 'Dragonite', type: 'Dragon/Flying', base_experience: 600 },
+			{ id: 143, name: 'Snorlax', type: 'Normal', base_experience: 540 },
+			{ id: 131, name: 'Lapras', type: 'Water/Ice', base_experience: 535 }
+		],
+		// Generation 2
+		gen2: [
+			{ id: 181, name: 'Ampharos', type: 'Electric', base_experience: 510 },
+			{ id: 208, name: 'Steelix', type: 'Steel/Ground', base_experience: 510 },
+			{ id: 229, name: 'Houndoom', type: 'Dark/Fire', base_experience: 500 },
+			{ id: 230, name: 'Kingdra', type: 'Water/Dragon', base_experience: 540 },
+			{ id: 248, name: 'Tyranitar', type: 'Rock/Dark', base_experience: 600 },
+			{ id: 160, name: 'Feraligatr', type: 'Water', base_experience: 530 }
+		],
+		// Generation 3
+		gen3: [
+			{ id: 254, name: 'Sceptile', type: 'Grass', base_experience: 530 },
+			{ id: 282, name: 'Gardevoir', type: 'Psychic/Fairy', base_experience: 518 },
+			{ id: 306, name: 'Aggron', type: 'Steel/Rock', base_experience: 530 },
+			{ id: 310, name: 'Manectric', type: 'Electric', base_experience: 475 },
+			{ id: 330, name: 'Flygon', type: 'Ground/Dragon', base_experience: 520 },
+			{ id: 350, name: 'Milotic', type: 'Water', base_experience: 540 }
+		],
+		// Mixed Generations
+		mixedGens: [
+			{ id: 149, name: 'Dragonite', type: 'Dragon/Flying', base_experience: 600 },
+			{ id: 6, name: 'Charizard', type: 'Fire/Flying', base_experience: 534 },
+			{ id: 94, name: 'Gengar', type: 'Ghost/Poison', base_experience: 500 },
+			{ id: 376, name: 'Metagross', type: 'Steel/Psychic', base_experience: 600 },
+			{ id: 9, name: 'Blastoise', type: 'Water', base_experience: 530 },
+			{ id: 248, name: 'Tyranitar', type: 'Rock/Dark', base_experience: 600 }
+		]
+	};
+
+	render() {
+		let hand1 = [ ...this.props.gen1 ];
+		let hand2 = [ ...this.props.gen2 ];
+		let hand3 = [ ...this.props.gen3 ];
+		let hand4 = [ ...this.props.mixedGens ];
+
+		let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+		let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+		let exp3 = hand3.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+		let exp4 = hand4.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+		return (
+			<div>
+				<Pokedex pokemon={hand1} exp={exp1} gen="Generation 1" />
+				<Pokedex pokemon={hand2} exp={exp2} gen="Generation 2" />
+				<Pokedex pokemon={hand3} exp={exp3} gen="Generation 3" />
+				<Pokedex pokemon={hand4} exp={exp4} gen="Mixed Generations" />
+			</div>
+		);
+	}
 }
 
 export default Pokegame;
