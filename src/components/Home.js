53f135f113cf620b06dfb21bb12aa33d567b8a3d
Victor Vargas <vvargasnunez@gmail.com>
2021-01-22 10:42:58 -0800

    Saved image to work environment

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import Message from './Message';
 import './Home.css';
+import ash from '../ash.jpg';
 
 class Home extends Component {
   render() {
@@ -9,17 +10,11 @@ class Home extends Component {
       <div>
         <Message>
           <h1 className="Home-h1">Pokemon Dream Team</h1>
-          <img className="Home-img" src="https://decider.com/wp-content/uploads/2016/07/pokemon-feature.jpg?quality=80&strip=all&w=978" alt="" />
+          <img className="Home-img" src={ash} alt="ash from pokemon" />
           <h3>I loved playing pokemon as a kid - though I stopped following the series closely after the Hoenn region (Generation 3)</h3>
           <h3>Check out my ideal pokemon teams I'd play with as a kid for each generation!</h3>
           <h3>Construct your own random team with my Random Party Maker page!</h3>
-        </Message>
-{/* 
-        <Link to='/gen1'>KANTO</Link>
-          <Link to='/gen2'>JOHTO</Link>
-          <Link to='/gen3'>HOENN</Link>
-          <Link to='/gen-mixed'>MIXED</Link> */}
-        
+        </Message>     
       </div>
     )
   }
